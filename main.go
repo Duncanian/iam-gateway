8ba29915d9b760c82aeb5d02190ca7f3a5ddb384
Ian Duncan <ianduncan08@gmail.com>
2019-05-07 19:51:24 +0300

    ch(auth): Create go-graphql client for creating a user
     - Add a protocol buffer
     - Install protoc & go-protoc-gen for compiling your protocol buffer
     - Create interfaces and connect to gRPC

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/handler"
 	"github.com/Duncanian/iam-gateway/server"
+	pb "github.com/Duncanian/iam-gateway/server/protobuf"
 	"github.com/Duncanian/iam-gateway/server/resolvers"
+	"google.golang.org/grpc"
 )
+
+func grpcConnection() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to start gRPC connection: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewSimpleServerClient(conn)
+
+	err, _ = client.CreateUser(context.Background(), &pb.GoogleIdToken{Token: "tevgvybububvvg"})
+	if err != nil {
+		log.Fatalf("Failed to create user: %v", err)
+	}
+	log.Println("Created user!")
+}
 
 const defaultPort = "8080"
 
@@ -17,6 +37,8 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
+
+	grpcConnection()
 
 	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
 	http.Handle("/query", handler.GraphQL(server.NewExecutableSchema(server.Config{Resolvers: &resolvers.Resolver{}})))
